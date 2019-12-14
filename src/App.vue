a8b34c9a73e2e8a6eabb4f87ef44ccde31df48d4
jonathanmcdonagh <jonathan.a.mcdonagh@gmail.com>
2019-12-14 08:29:30 +0000

    Made some small changes

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
         <b-navbar-nav>
           <b-nav-item to="/"><i class="fa fa-graduation-cap" style="padding: 5px"> Home</i></b-nav-item>
           <b-nav-item to="/items"><i class="fa fa-list-ul" style="padding: 5px"> Manage Lost Items</i></b-nav-item>
-          <b-nav-item data-test="lostitembtn" to="/lostitem"><i class="fa fa-plus-square-o" style="padding: 5px"> Report Lost Item</i></b-nav-item>
+          <b-nav-item to="/lostitem"><i class="fa fa-plus-square-o" style="padding: 5px"> Report Lost Item</i></b-nav-item>
           <b-nav-item to="/map"><i class="fa fa-map-marker" style="padding: 5px"> Map</i></b-nav-item>
           <b-nav-item to="/about"><i class="fa fa-user-secret" style="padding: 5px"> About Us</i></b-nav-item>
         </b-navbar-nav>
@@ -29,10 +29,8 @@
 import firebase from 'firebase'
 import Toasted from 'vue-toasted'
 import Vue from 'vue'
-
 // eslint-disable-next-line no-undef
 Vue.use(Toasted)
-
 export default {
   name: 'App',
   data () {
@@ -76,47 +74,37 @@ export default {
     text-align: center;
     color: #2c3e50;
   }
-
   .VueTables__sortable {
     cursor: pointer;
   }
-
   .bg-dark {
     background-color: grey !important;
   }
-
   i.fa.fa {
     color: white;
   }
-
   li.nav-item:hover {
     background-color: #0D3C51;
     color: #ffffff;
   }
-
   a.navbar-brand.router-link-exact-active.router-link-active {
     color: #E82025;
   }
-
   .userName {
     color: #E82025;
   }
-
   .nav-link.router-link-exact-active.router-link-active {
     text-decoration: underline;
   }
-
   .navlogo {
     padding: 5px;
     height: 50px;
     width: 50px;
   }
-
   #__BVID__47__BV_button_ > span {
     font-weight: bold;
     color: white;
   }
-
   ul.dropdown-menu.dropdown-menu-right.show {
     background-color: grey;
   }
